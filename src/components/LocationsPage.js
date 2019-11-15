7b75142876288effe39a16ad1222bcb41a4d868b
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-15 14:16:25 -0500

    rewrite LocationsPage to match CharactersPage

--- src/components/LocationsPage.js
+++ src/components/LocationsPage.js
@@ -2,6 +2,8 @@
 import React from 'react';
 
 /// internal modules ///
+import locationAPI from '../data/rickandmortyapi.com/location-api';
+import { fetchData , getPage } from '../data/fetch';
 import SearchForm from './SearchForm';
 import LocationsList from './LocationsList';
 
@@ -9,6 +11,8 @@ import LocationsList from './LocationsList';
   STATES
 ***************************************/
 const init = {
+  'query' : {},
+  'page' : 1,
   'info' : {},
   'locations' : [],
 };
@@ -17,14 +21,33 @@ const init = {
   COMPONENT
 ***************************************/
 const LocationsPage = (props) => {
+  /// states ///
+  const [query , setQuery] = React.useState (init.query);
+  const [page , setPage] = React.useState (init.page);
+  const [info , setInfo] = React.useState (init.info);
+  const [locations , setLocations] = React.useState (init.locations);
+
+  /// virtual states ///
+  const setData = (data) => {
+    setPage (getPage (data.info));
+    setInfo (data.info);
+    setLocations (data.results);
+  }
+
+  /// effects ///
+  React.useEffect(() => {
+    fetchData (locationAPI , init.locations) (query , setData);
+  }, [query]);
+
+  /// thing ///
   return (
     <section className="locations-page">
       <header>
         <h1>Locations</h1>
       </header>
       <main>
-        <SearchForm/>
-        <LocationsList/>
+        <SearchForm search={setQuery}/>
+        <LocationsList list={locations}/>
       </main>
     </section>
   );
