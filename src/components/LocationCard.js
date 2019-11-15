9e50f902f8b461358eeb1907e257437ff28b8916
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-15 14:13:58 -0500

    rewrite LocationCard to match CharacterCard

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,12 +1,18 @@
 /// external modules ///
 import React from 'react';
+import { Card, CardImg , CardBody , CardTitle } from 'reactstrap';
 
 /***************************************
   COMPONENT
 ***************************************/
-const LocationCard = ({ name, type  , dimension , residents , ...props }) => {
+const LocationCard = ({ data , ...props }) => {
   return (
-    <div>TODO: location</div>
+    <Card className='location'>
+      <CardImg top width='100%' src={data.image} alt='location image'/>
+      <CardBody>
+        <CardTitle>{data.name}</CardTitle>
+      </CardBody>
+    </Card>
   );
 };
 
