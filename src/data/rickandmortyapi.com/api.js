2f56716b778eb29d76f68c09a3f2fda6bffc3042
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-15 12:55:28 -0500

    create API files for rickandmortyapi.com

---
+++ src/data/rickandmortyapi.com/api.js
@@ -0,0 +1,8 @@
+const API = {
+  "base_url" : "https://rickandmortyapi.com/api/",
+  "query_url" : function (query) {
+    return (`${this.base_url}`);
+  },
+}
+
+export default API;
---
+++ src/data/rickandmortyapi.com/character-api.js
@@ -0,0 +1,18 @@
+import RNMAPI from "./api";
+
+const API = {
+  "base_url" : `${RNMAPI.base_url}character/`,
+  "query_url" : function (query) {
+    let params = "";
+    // params are mutually exclusive
+    if (query.page !== undefined) {
+      params = `?page=${query.page}`;
+    }
+    else if (query.id !== undefined) {
+      params = `${query.id}/`;
+    }
+    return (`${this.base_url}${params}`);
+  },
+};
+
+export default API;
